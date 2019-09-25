c313d00428a46e4ec2ee8884333dd41ec75cfcaa
syntomic <zhouhaifengmath@gmail.com>
2019-09-25 21:54:44 +0800

    commit

--- myblog/settings.py
+++ myblog/settings.py
@@ -40,7 +40,7 @@ class TestingConfig(BaseConfig):
 
 
 class ProductionConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URI = prefix + os.path.join(basedir, 'data-dev.db')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', prefix + os.path.join(basedir, 'data-dev.db'))
 
 
 config = {
